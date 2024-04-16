db92d1803552bf2976dfe506ef8013db0dead9fc
eocone <66829337+HoliGit@users.noreply.github.com>
2024-04-16 16:06:38 +0200

    Add files via upload

---
+++ myITour/iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by EO on 17/01/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
